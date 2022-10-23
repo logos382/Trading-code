8fb3a7835c9f70fb893a1421e6c154052f6c1cd6
logos382 <logos382@mymail.com>
2022-10-23 17:14:20 +0200

    23/10/2022

--- pyCode/retrive_store.py
+++ pyCode/retrive_store.py
@@ -1,54 +1,41 @@
-"A script that retrive and store data feed from live service"
+import asyncio
+import pandas as pd
+import sqlalchemy
+from binance import AsyncClient, BinanceSocketManager
 
-import websocket
-import json
-import _thread
-import time
-import rel
+engine = sqlalchemy.create_engine('sqlite:///Trading-code/sqldb/B_Crypto.db')
 
+async def createdf(msg):
+    df = pd.DataFrame([msg])
+    df = df.loc[:,['s', 'E', 'p']]
+    df.columns = ['Symbol', 'Time', 'Price']
+    df.Price = df.Price.astype(float)
+    df.Time = pd.to_datetime(df.Time, unit='ms')
+    print(df)
+    return df
 
-def on_open(ws):
-    data = json.dumps({"op": "subscribe",
-                       "channel": "ticker",
-                       "market": "BTC-PERP"})
-    ws.send(data)
-    print("Connected")
+async def writesql(msg, symbol):
+    frame = await createdf(msg)
+    frame.to_sql(symbol, engine, if_exists='append', index=False)
 
-def on_message(ws, message):
-    print(message)
-
-def on_error(ws, error):
-    print(f"Error: {error}")
-
-def on_close(close_msg):
-    print(f"Connection close")
+async def main(n, symbol):
+    # initialise the client
+    client = await AsyncClient.create()
+    # initialise websocket factory manager
+    bsm = BinanceSocketManager(client)
+    # create listener using async with
+    # this will exit and close the connection after 5 messages
+    # start any sockets here, i.e a trade socket
+    async with bsm.trade_socket(symbol) as ts:
+        for _ in range(n):
+            msg = await ts.recv()
+            #print(msg)
+            loop.call_soon(asyncio.create_task, writesql(msg, symbol))
+    # exit the context manager
+    await client.close_connection()
 
 if __name__ == "__main__":
 
-    # try:
-    #     while True:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(main(5, 'BTCBUSD'))
 
-    #         endpoint = "wss://ftx.com/ws/"
-
-    #         websocket.enableTrace(True)
-
-    #         ws = websocket.WebSocketApp(endpoint, on_open=on_open, on_message=on_message, on_error=on_error, on_close=on_close)
-
-    #         # ws.run_forever(ping_interval=15)    # Set a ping to server every 15s
-    #         ws.run_forever()
-
-    # except KeyboardInterrupt:
-    #     ws.close()
-
-    endpoint = "wss://ftx.com/ws/"
-
-    websocket.enableTrace(True)
-
-    ws = websocket.WebSocketApp(endpoint, on_open=on_open, on_message=on_message, on_error=on_error, on_close=on_close)
-
-    # ws.run_forever(dispatcher=rel)    # Set a ping to server every 15s
-    ws.run_forever()    
-    
-    # rel.signal(2, rel.abort(), ws.close())  # Keyboard Interrupt
-    
-    # rel.dispatch()
