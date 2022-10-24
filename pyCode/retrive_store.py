3c24f9e80357e1a4321751ae19200136fcb62c23
logos382 <logos382@mymail.com>
2022-10-24 10:28:25 +0200

    24/10/2022

--- pyCode/retrive_store.py
+++ pyCode/retrive_store.py
@@ -6,21 +6,48 @@ from binance import AsyncClient, BinanceSocketManager
 
 engine = sqlalchemy.create_engine('sqlite:///Trading-code/sqldb/B_Crypto.db')
 
-
 async def createdf(msg):
+    """_summary_
+
+    Args:
+        msg (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    # Create a Data Frame from websocket msg
     df = pd.DataFrame([msg])
+    # Slice Dataframe to keep only required data
     df = df.loc[:,['s', 'E', 'p']]
+    # Meaningfully rename colums
     df.columns = ['Symbol', 'Time', 'Price']
+    # Convert Text to float for future calculations
     df.Price = df.Price.astype(float)
+    # convert unix UTC time to more readable one
     df.Time = pd.to_datetime(df.Time, unit='ms')
     print(df)
     return df
 
 async def writesql(msg, symbol):
+    """_summary_
+
+    Args:
+        msg (_type_): _description_
+        symbol (_type_): _description_
+    """
+    # save the Dataframe with Websocket msg nto a variable
     frame = await createdf(msg)
+    # write the Dataframe into sqlite database
     frame.to_sql(symbol, engine, if_exists='append', index=False)
 
 async def main(symbol, runtime):
+    """_summary_
+
+    Args:
+        symbol (_type_): _description_
+        runtime (integer): an integer representing the number of seconds we listen to websocket before we close the connection
+                           e.g. 60 = 1 minute, 3600 = 1 hour, 86400 = 1 day, 2592000 = 30 Days, 31536000 = 1 year
+    """
     # initialise the client
     client = await AsyncClient.create()
     # initialise websocket factory manager
@@ -29,14 +56,18 @@ async def main(symbol, runtime):
     # this will exit and close the connection after 5 messages
     # start any sockets here, i.e a trade socket
     async with bsm.trade_socket(symbol) as ts:
-        #for _ in range(n):
+        # save start and end current time into variables
         starttime = time.time()
-        endtime = time.time()
-        while endtime < starttime + runtime:
+        currenttime = time.time()
+        # Start a while loop with base case
+        while currenttime < starttime + runtime:
+            # save the received Websocket msg into a variable
             msg = await ts.recv()
-            #print(msg)
+            # call a coroutine to excute the writing into sql wile waiting for next msg
+            # the attempt here is to continue to listen websocket while writing data
             loop.call_soon(asyncio.create_task, writesql(msg, symbol))
-            endtime = time.time()
+            # update the current time to evaluate loop continuation
+            currenttime = time.time()
     # exit the context manager
     await client.close_connection()
 
@@ -44,5 +75,5 @@ if __name__ == "__main__":
 
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(main('BTCBUSD', 600))
+    loop.run_until_complete(main('BTCBUSD', 36000))
 
