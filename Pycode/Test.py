0b57e450ef76141691df92a9b551a4bda15617ff
logos382 <logos382@mymail.com>
2023-10-18 15:14:39 +0200

    18/10/2023

--- Pycode/Test.py
+++ Pycode/Test.py
@@ -12,13 +12,13 @@ engine = sqlalchemy.create_engine('sqlite:///Trading-code/Sqldb/B_Crypto.db', po
 Session = sessionmaker(bind=engine)
 
 starttime = time.time()
-runtime = 600 # 60 = 1m; 3600 = 1H; 86700 = 1D; 607800 = 1W
+runtime = 3600*3 # 60 = 1m; 3600 = 1H; 86700 = 1D; 607800 = 1W
 
 exchanges = {
         'mexc': ['BTC/USDT', 'RUNE/USDT'],
     }
 
-tablenames = ['RUNE_USDT_MEXCGlobal']
+tablenames = ['RUNE_USDT_MEXCGlobal', 'BTC_USDT_MEXCGlobal']
 
 async def createdf(msg):
     """_summary_
@@ -55,8 +55,8 @@ async def writesql(msg, symbol, exchange):
     tablename = symbol.replace("/", "_")+"_"+str(exchange).replace(" ", "")
     if insp.has_table(tablename):
         df = pd.read_sql('SELECT id FROM '+ tablename, con=engine)
-        lastrow = df.tail(10)
-        if frame["id"].values not in lastrow["id"].values :
+        last10rows = df.tail(10)
+        if frame["id"].values not in last10rows["id"].values :
             session = Session()  # Create a session manually
             # Perform batch insert within a transaction 
             transaction = session.begin()
@@ -80,18 +80,24 @@ async def writesql(msg, symbol, exchange):
             # Proper error handling implementation...
 
 
-async def readsql(tablenames, runtime):
+async def readsql(runtime, tablenames):
+    # tablenames = ['RUNE_USDT_MEXCGlobal', 'BTC_USDT_MEXCGlobal']
     while True:
         insp = inspect(engine)
         for tablename in tablenames:
             if insp.has_table(tablename):
+                # df = pd.read_sql('SELECT * FROM '+ tablename + ' ORDER BY CloseTime DESC LIMIT 100', con=engine)
                 df = pd.read_sql('SELECT * FROM '+ tablename, con=engine)
-                lastrow = df.tail(1)
-                # print(lastrow, 'readsql')
-                await sleep(5)
+                df['CloseTime'] = pd.to_datetime(df['CloseTime'])
+                df = df.set_index(['CloseTime'])
+                df = df['LastPrice'].resample('5min').agg(Open="first", Close="last",High="max", Low="min")
+                df['Symbol'] = tablename
+                print(df.tail(1), 'readsql')
+                # await sleep(2)
         currenttime = time.time()
         if currenttime >= starttime + runtime:
             break
+        return df
 
 
 async def symbol_loop(exchange, symbol, runtime):
@@ -101,8 +107,8 @@ async def symbol_loop(exchange, symbol, runtime):
             msg = await exchange.fetch_trades(symbol, limit=1)
             now = exchange.milliseconds()
             # print(exchange.iso8601(now), exchange.id, symbol, orderbook['asks'][0], orderbook['bids'][0])
-            print(exchange.iso8601(now), exchange.id, symbol, msg)
-            await writesql(msg, symbol, exchange)
+            # print(exchange.iso8601(now), exchange.id, symbol, msg)
+            await gather (writesql(msg, symbol, exchange), readsql(runtime, tablenames))
         except Exception as e:
             print(str(e))
             # raise e  # uncomment to break all loops in case of an error in any one of them
@@ -116,17 +122,17 @@ async def exchange_loop(exchange_id, symbols, runtime):
     print('Starting the', exchange_id, 'exchange loop with', symbols)
     exchange = getattr(ccxt, exchange_id)()
     exchange.enableRateLimit = True
-    loops = [symbol_loop(exchange, symbol, runtime) for symbol in symbols]
-    await gather(*loops)
+    coroloops = [symbol_loop(exchange, symbol, runtime) for symbol in symbols]
+    await gather(*coroloops)
     await exchange.close()
 
 
-async def main(exchanges, runtime, tablenames):
-        loops = [exchange_loop(exchange_id, symbols, runtime) for exchange_id, symbols in exchanges.items()]
-        # await gather(*loops,readsql(tablenames, runtime))
-        await gather(*loops)
+async def main(exchanges, runtime):
+        coroloops = [exchange_loop(exchange_id, symbols, runtime) for exchange_id, symbols in exchanges.items()]
+        await gather(*coroloops)
+        # await gather(*loops)
 
 
 
 if __name__ == "__main__":
-    run(main(exchanges, runtime, tablenames))
+    run(main(exchanges, runtime))
