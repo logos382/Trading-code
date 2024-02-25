2ec20fa0852ddad6882e0a1b1583d7db36ea936a
logos382 <logos382@mymail.com>
2024-02-25 11:41:45 +0100

    25/02/2024

--- Pycode/AsyncRobot.py
+++ Pycode/AsyncRobot.py
@@ -1,32 +1,53 @@
 import asyncio
 
-class aRobot():
+class aRobot:
 
     def __init__(self) -> None:
+        self.loop = asyncio.new_event_loop()
         self.isrunning = True
+        self.tasks = {}
+
+    async def asleep(self) -> None:
+        await asyncio.sleep(20)
+        print("asleep completed")
+        
 
     async def start(self) -> None:
         while self.isrunning:
-            with asyncio.TaskGroup() as tg:
+            async with asyncio.TaskGroup() as tg:
                 task1 = tg.create_task(self.menu())
-                task2 = tg.create_task(self.get_input())      
+                task2 = tg.create_task(self.get_input())
+
 
     async def menu(self) -> None:
         print("1. Add task1")
-        print("2. Add task2")
         print("3. Check all tasks")
-        print("4. ")
         print("5. Exit")
 
     async def get_input(self):
-        usr_input = input('==>')
-        return usr_input
+        usr_input = input('==> ')
+        await self.select_task(int(usr_input))
+    
+    async def select_task(self,input: int):
+        if input == 1:
+            self.schedule_task(self.asleep)
+        elif input == 3:
+            await self.check_tasks()
+        elif input == 5:
+            self.isrunning = False
+        else:
+            pass
 
-
-
-
+    async def schedule_task(self, coro_func):
+        task = self.loop.create_task(coro_func)
+ 
+    async def check_tasks(self):
+        tasks = asyncio.all_tasks()
+        for task in tasks:
+            print(f'> {task.get_name()}, {task.get_coro()}')
 
 
 if __name__ == "__main__":
 
-    asyncio.run(aRobot.start())
+    robot = aRobot()
+    asyncio.run(robot.start())
